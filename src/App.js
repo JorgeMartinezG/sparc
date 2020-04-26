a55a39ada25d707dbff99d534c3c1648098ac101
Jorge Martinez <jorgemg0327@gmail.com>
2020-04-26 11:26:56 -0500

    Used transitions for rendering search menu

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
-import React from "react";
-import { Button } from "@wfp/ui";
+import React, { useRef } from "react";
 
 import LogoEmblem from "./assets/wfp-logo-emblem-white.svg";
 import { iconSearch } from "@wfp/icons";
+import { Icon } from "@wfp/ui";
+
 import { Modal } from "@wfp/ui";
 
 import "./assets/style.scss";
 import "@wfp/ui/assets/css/styles.min.css";
 
+const SearchMenu = ({ trigger }) => {
+  return (
+    <div ref={trigger} className="bg-gray search">
+      AAAAA
+    </div>
+  );
+};
+
+const SearchButton = ({ trigger }) => {
+  const toggleSearch = () => {
+    trigger.current.classList.toggle("is_open");
+  };
+
+  return (
+    <div
+      onClick={toggleSearch}
+      className="pl2 pv2 hover-interactive-02 link pointer flex items-center w-25 tc"
+    >
+      <span>Search</span>
+      <Icon className="ml2 fill-white h1 w1" icon={iconSearch} />
+    </div>
+  );
+};
+
+const Search = () => {
+  const trigger = useRef();
+
+  return (
+    <div>
+      <SearchButton trigger={trigger} />
+      <SearchMenu trigger={trigger} />
+      <div className="bg-gray">BBBBB</div>
+    </div>
+  );
+};
+
 const About = () => {
   return (
     <div className="tr ph2 pv1">
       <p className="white f6 b link pointer dim">About</p>
-      <Modal open={true} passiveModal={true}>
+      <Modal open={false} passiveModal={true}>
         <p className="wfp--modal-content__text">Nothing to see here</p>
       </Modal>
     </div>
@@ -22,7 +59,7 @@ const About = () => {
 const Nav = () => {
   return (
     <div className="white f6 b">
-      <Button icon={iconSearch}>Search</Button>
+      <Search />
     </div>
   );
 };
@@ -30,7 +67,7 @@ const Nav = () => {
 const Logo = () => {
   return (
     <div className="flex items-center w-100 justify-center pv2">
-      <img className="h3 w3 mr3" src={LogoEmblem} />
+      <img alt="emblem" className="h3 w3 mr3" src={LogoEmblem} />
       <p className="white ttu b f3">sparc</p>
     </div>
   );
@@ -38,7 +75,7 @@ const Logo = () => {
 
 const Header = () => {
   return (
-    <div className="w-20 bg-primary">
+    <div className="w-20 bg-interactive-01">
       <About />
       <Logo />
       <Nav />
