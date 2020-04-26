260dc9c8e6dbfecd06d25a96d385d20a8eeb4abe
Jorge Martinez <jorgemg0327@gmail.com>
2020-04-25 20:12:59 -0500

    Test with tachyons and logo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from "react";
 import { Button } from "@wfp/ui";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbHome } from "@wfp/ui";
-import "./style.scss";
 
+import LogoEmblem from "./assets/wfp-logo-emblem-white.svg";
+
+import "./assets/style.scss";
 import "@wfp/ui/assets/css/styles.min.css";
+
+const Sidebar = () => {
+  return (
+    <div className="w-25 h4 bg-primary">
+      <img className="h3 w3" src={LogoEmblem} />
+      <div></div>
+    </div>
+  );
+};
 
 const App = () => {
   return (
-    <div className="container">
-      <h1>hello</h1>
-      <Breadcrumb>
-        <BreadcrumbItem>
-          <a href="/#">
-            <BreadcrumbHome />
-          </a>
-        </BreadcrumbItem>
-        <BreadcrumbItem href="#">Breadcrumb 2</BreadcrumbItem>
-        <BreadcrumbItem disableLink>Breadcrumb 3</BreadcrumbItem>
-        link
-      </Breadcrumb>
+    <div className="vh-100">
+      <Sidebar />
     </div>
   );
 };
