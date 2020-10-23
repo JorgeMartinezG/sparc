c7bb039ba2a7a15d2d9ad5bca101a8067ae2f238
Jorge Martinez <jorgemg0327@gmail.com>
2020-10-23 10:07:09 -0500

    Initial work on pdf rendering

--- src/components/icons.js
+++ src/components/icons.js
@@ -1,18 +1,44 @@
-import React from "react";
-import { iconMenu } from "@wfp/icons";
+import React, { useMemo } from "react";
+import { iconMenu, iconPrintGlyph } from "@wfp/icons";
+import { PDFDownloadLink } from "@react-pdf/renderer";
 import { Icon } from "@wfp/ui";
+import { PdfPrint } from "./pdf.js";
+
+const MemoComponent = () => {
+  return useMemo(
+    () => (
+      <PDFDownloadLink document={<PdfPrint />} fileName="somename.pdf">
+        {({ blob, url, loading, error }) =>
+          loading ? null : (
+            <Icon
+              className="ml2 h2 shadow-2 w2 pa2 br-100 bg-light-gray ba1"
+              icon={iconPrintGlyph}
+            />
+          )
+        }
+      </PDFDownloadLink>
+    ),
+    []
+  );
+};
 
 export const Icons = ({ sidebarRef }) => {
   return (
-    <Icon
-      className="ml2 h2 shadow-2 w2 absolute z-3 left-0 mt3 pa2 br-100 bg-light-gray ba1"
-      icon={iconMenu}
-      onClick={() =>
-        ["dn", "flex", "flex-column"].map((i) => {
-          sidebarRef.current.classList.toggle(i);
-          return null;
-        })
-      }
-    />
+    <div>
+      <Icon
+        className="ml2 h2 shadow-2 w2 absolute z-3 left-0 mt3 pa2 br-100 bg-light-gray ba1"
+        icon={iconMenu}
+        onClick={() =>
+          ["dn", "flex", "flex-column"].map((i) => {
+            sidebarRef.current.classList.toggle(i);
+            return null;
+          })
+        }
+      />
+
+      <div style={{ top: "4.5em" }} className="z-4 absolute ">
+        <MemoComponent />
+      </div>
+    </div>
   );
 };
